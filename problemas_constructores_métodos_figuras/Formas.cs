dc32bde740978eb1db1b4ba31486b058d33c1fd5
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-09-28 22:31:46 -0500

    Initial commit

---
+++ problemas_constructores_métodos_figuras/Formas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problemas_constructores_métodos_figuras
+{
+    internal class Formas
+    {
+        //Propiedades
+        private int Area;
+        private int Base;
+        private int Altura;
+        //Constructor
+        public Formas()
+        {
+            Area = 6;
+            Base = 10;
+            Altura = 100;
+        }
+        //Metodos
+        public void Imprimir_area_Triangulo()
+        {
+            Area = (Base * Altura) / 2;
+            Console.WriteLine("El area del Triangulo es: " + Area);
+        }
+        public void Imprimir_area_Trapecio()
+        {
+            Area = (Base + Base * Altura) / 2; 
+            Console.WriteLine("El area del Trapecio es: " + Area);
+        }
+    }
+}
---
+++ problemas_constructores_métodos_figuras/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using problemas_constructores_métodos_figuras;
+
+Console.WriteLine("Hello, Este programas es de imprimir áreas de Formas!");
+Formas forms = new Formas();
+forms.Imprimir_area_Triangulo();
+forms.Imprimir_area_Trapecio();
